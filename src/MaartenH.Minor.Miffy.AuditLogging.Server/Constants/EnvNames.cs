c88e81c0cfdd344d9052ddced1a1a821fedffa6d
Maarten van der Heijden <djbatcat@gmail.com>
2019-12-05 13:50:20 +0100

    master: Add static keyword

--- src/MaartenH.Minor.Miffy.AuditLogging.Server/Constants/EnvNames.cs
+++ src/MaartenH.Minor.Miffy.AuditLogging.Server/Constants/EnvNames.cs
@@ -1,6 +1,6 @@
 namespace MaartenH.Minor.Miffy.AuditLogging.Server.Constants
 {
-    public class EnvNames
+    internal static class EnvNames
     {
         internal const string DatabaseConnectionString = "DB_CONNECTION_STRING";
     }
