38caf4bb2f1a1952b4b698f10b66d2cc39643e22
Maarten van der Heijden <djbatcat@gmail.com>
2019-12-09 21:37:39 +0100

    master: Update migration

--- src/MaartenH.Minor.Miffy.AuditLogging.Server/Migrations/20191209203722_Rename EventType to Type.Designer.cs
+++ src/MaartenH.Minor.Miffy.AuditLogging.Server/Migrations/20191209203722_Rename EventType to Type.Designer.cs
@@ -2,14 +2,16 @@
 using MaartenH.Minor.Miffy.AuditLogging.Server.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MaartenH.Minor.Miffy.AuditLogging.Server.Migrations
 {
     [DbContext(typeof(AuditLogContext))]
-    partial class AuditLogContextModelSnapshot : ModelSnapshot
+    [Migration("20191209203722_Rename EventType to Type")]
+    partial class RenameEventTypetoType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/MaartenH.Minor.Miffy.AuditLogging.Server/Migrations/20191209203722_Rename EventType to Type.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MaartenH.Minor.Miffy.AuditLogging.Server.Migrations
+{
+    public partial class RenameEventTypetoType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EventType",
+                table: "AuditLogItems");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "AuditLogItems",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "AuditLogItems");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EventType",
+                table: "AuditLogItems",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+        }
+    }
+}
