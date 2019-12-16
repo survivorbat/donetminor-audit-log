696ad63cad4bba84099b408ae84230333581e309
Maarten van der Heijden <djbatcat@gmail.com>
2019-12-16 10:39:36 +0100

    master: Remove events from coverage

--- MaartenH.Minor.Miffy.AuditLogging/Events/EndReplayEvent.cs
+++ MaartenH.Minor.Miffy.AuditLogging/Events/EndReplayEvent.cs
@@ -1,11 +1,19 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 using MaartenH.Minor.Miffy.AuditLogging.Constants;
 using Minor.Miffy.MicroServices.Events;
 
 namespace MaartenH.Minor.Miffy.AuditLogging.Events
 {
+    /// <summary>
+    /// Empty event that is sent to indicate the end of a replay
+    /// </summary>
+    [ExcludeFromCodeCoverage]
     public class EndReplayEvent : DomainEvent
     {
+        /// <summary>
+        /// Instantiate a replayend event with a process id
+        /// </summary>
         public EndReplayEvent(Guid processId) : base(ReplayTopicNames.ReplayEndEventTopic, processId)
         {
         }
--- MaartenH.Minor.Miffy.AuditLogging/Events/StartReplayEvent.cs
+++ MaartenH.Minor.Miffy.AuditLogging/Events/StartReplayEvent.cs
@@ -1,11 +1,19 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 using MaartenH.Minor.Miffy.AuditLogging.Constants;
 using Minor.Miffy.MicroServices.Events;
 
 namespace MaartenH.Minor.Miffy.AuditLogging.Events
 {
+    /// <summary>
+    /// Empty event that is sent to indicate the start of a replay
+    /// </summary>
+    [ExcludeFromCodeCoverage]
     public class StartReplayEvent : DomainEvent
     {
+        /// <summary>
+        /// Instantiate a replaystart event with a process id
+        /// </summary>
         public StartReplayEvent(Guid processId) : base(ReplayTopicNames.ReplayStartEventTopic, processId)
         {
         }
